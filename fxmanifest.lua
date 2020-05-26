306c3e4e96a3829d0dee4e8e0ba88db5e878fd82
Akkariin Meiko <akkariins@gmail.com>
2020-05-26 20:45:52 +0800

    Update and rename __resource.lua to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,10 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'bodacious'
+games { 'gta5' }
+
 description "Forza Horizon 4 Minimap"
 author "Akkariin"
 url "https://www.zerodream.net/"
+version "1.1"
 
 client_script 'client.lua'
 server_script 'server.lua'
