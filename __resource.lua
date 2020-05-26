908c54eda00cf81b9d27baaa07cd2944e83d7c53
kasuganosoras <kasuganosora@tcotp.cn>
2020-05-26 19:37:40 +0800

    Init

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+description "Forza Horizon 4 Minimap"
+author "Akkariin"
+url "https://www.zerodream.net/"
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+files {
+	"hud.html"
+}
+
+ui_page "hud.html"
---
+++ client.lua
@@ -0,0 +1,12 @@
+Citizen.CreateThread(function()
+    local minimap = RequestScaleformMovie("minimap")
+    SetRadarBigmapEnabled(true, false)
+    Wait(0)
+    SetRadarBigmapEnabled(false, false)
+    while true do
+        Wait(0)
+        BeginScaleformMovieMethod(minimap, "SETUP_HEALTH_ARMOUR")
+        ScaleformMovieMethodAddParamInt(3)
+        EndScaleformMovieMethod()
+    end
+end)
